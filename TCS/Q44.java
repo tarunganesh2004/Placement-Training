5092d411b61509bd0c37f63ea599e9067fa809ab
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2024-07-08 12:52:20 +0530

    Q44

--- TCS/Q44.java
+++ TCS/Q44.java
@@ -6,7 +6,7 @@ public class Q44 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] a = new int[n];
+        int[] a = new int[n]; // 1,0,1,1,1 --> 11000 --> 11111
         for (int i = 0; i < n; i++) {
             a[i] = sc.nextInt();
         }
@@ -14,7 +14,29 @@ public class Q44 {
         sc.close();
     }
 
-    public static int leastNumber(int[] a) {
-        int n = a.length;
+    public static int leastNumber(int[] a) { //
+        int presses = 0;
+        int currentState = 1; // all doors open
+        for (int door : a) {
+            if (door != currentState) {
+                presses++;
+                currentState = 1 - currentState;
+            }
+        }
+        return presses;
+    }
+    
+    // another method O(n^2)
+    public static int leastNumber2(int[] a) {
+        int presses = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == 0) {
+                presses++;
+                for (int j = i; j < a.length; j++) {
+                    a[j] = 1 - a[i]; // toggle
+                }
+            }
+        }
+        return presses;
     }
 }
