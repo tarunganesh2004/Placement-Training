8edb5109c7e56be1bb9ef9dbd121ca33106be72c
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2024-07-09 10:48:20 +0530

    59

--- TCS/Q59.java
+++ TCS/Q59.java
@@ -29,4 +29,23 @@ public class Q59 {
         }
         return res.toString();
     }
+
+    public static String containsMissingAnother(String s) {
+        s = s.toLowerCase();
+        int[] a = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                a[ch - 'a']++;
+            }
+        }
+
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            if (a[i] == 0) {
+                res.append((char) (i + 'a'));
+            }
+        }
+        return res.toString();
+    }
 }
